03f441432a7e7c53832419e003490d81818b7644
Neil Ricci <iccir919@gmail.com>
2024-10-05 16:14:22 -0700

    Complete requirements for Roman Numeral Converter project

--- roman-numeral-converter/roman-numeral-converter.md
+++ roman-numeral-converter/roman-numeral-converter.md
@@ -14,5 +14,8 @@ Build an app that is functionally similar to [https://roman-numeral-converter.fr
 - [x] You should have a `div`, `span` or `p` element with an `id` of `output`
 - [x] When you click on the `#convert-btn` element without entering a value into the `#number` element, the `#output` element should contain the text `"Please enter a valid number"`
 - [x] When the `#number` element contains the number `-1` and the `#convert-btn` element is clicked, the `#output` element should contain the text `"Please enter a number greater than or equal to 1"`
-- [ ] When the `#number` element contains the number `4000` or greater and the `#convert-btn` element is clicked, the `#output` element should contain the text `"Please enter a number less than or equal to 3999"`
-- [ ]
+- [x] When the `#number` element contains the number `4000` or greater and the `#convert-btn` element is clicked, the `#output` element should contain the text `"Please enter a number less than or equal to 3999"`
+- [x] When the `#number` element contains the number `9` and the `#convert-btn` element is clicked, the `#output` element should contain the text `"IX"`
+- [x] When the `#number` element contains the number `16` and the `#convert-btn` element is clicked, the `#output` element should contain the text `"XVI"`
+- [x] When the `#number` element contains the number `649` and the `#convert-btn` element is clicked, the `#output` element should contain the text `"DCXLIX"`
+- [x] When the `#number` element contains the number `3999` and the `#convert-btn` element is clicked, the `#output` element should contain the text `"MMMCMXCIX"`
--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -3,40 +3,66 @@ const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-const checkUserInput = () => {
-    const inputNum = parseInt(numberInput.value);
-
-    if (!inputNum || isNaN(inputNum)) {
-        output.textContent = "Please enter a valid number";
-    } else if (inputNum < 1) {
-        output.textContent = "Please enter a number greater than or equal to 1";
-    } else if (inputNum > 3999) {
-        output.textContent = "Please enter a number less than or equal to 3999";
+const handleUserInput = (e) => {
+    output.textContent = "";
+    const userInput = numberInput.value;
+    if (isValidInput(userInput)) {
+        output.textContent = numberToRomanNumber(userInput).toString();
     }
 }
 
-const romanNumberToNumber = [
+const isValidInput = (userInput) => {
+    if (!userInput || isNaN(userInput)) {
+        output.textContent = "Please enter a valid number";
+        return false;
+    } else if (userInput < 1) {
+        output.textContent = "Please enter a number greater than or equal to 1";
+        return false;
+    } else if (userInput > 3999) {
+        output.textContent = "Please enter a number less than or equal to 3999";
+        return false
+    } else {
+        return true;
+    }
+}
+
+const numberToRomanNumberMap = [
     [1, "I"],
+    [4, "IV"],
     [5, "V"],
+    [9, "IX"],
     [10, "X"],
+    [40, "XL"],
     [50, "L"],
+    [90, "XC"],
     [100, "C"],
+    [400, "CD"],
     [500, "D"],
+    [900, "CM"],
     [1000, "M"]
 ]
 
 const numberToRomanNumber = (number) => {
     let result = "";
 
-    while (number > 0) {
-        
+    while (number != 0) {
+        for(let i = numberToRomanNumberMap.length - 1; i >= 0; i--) {
+            let currentNumber = numberToRomanNumberMap[i][0];
+            if (number >= currentNumber) {
+                result += numberToRomanNumberMap[i][1];
+                number -= currentNumber;
+                break;
+            }
+        }
     }
+
+    return result;
 }
 
-convertBtn.addEventListener("click", checkUserInput);
+convertBtn.addEventListener("click", handleUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        checkUserInput();
+        handleUserInput();
     }
 })
